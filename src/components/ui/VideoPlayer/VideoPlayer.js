dd1a7aec6e46bccc271abec4a3e24b2c6db89cfc
Leonardo <hgtleohgt@gmail.com>
2025-08-15 20:11:59 -0300

    retirando video teste

--- src/components/ui/VideoPlayer/VideoPlayer.js
+++ src/components/ui/VideoPlayer/VideoPlayer.js
@@ -9,20 +9,9 @@ const VideoPlayer = ({ videoId }) => {
 
   return (
     <div>
-      {!isPlaying ? (
-        <button onClick={handlePlayVideo}>
-          <img src="./images/utils/iconPlay.png" />
-        </button>
-      ) : (
-        <iframe
-          width="600"
-          height="340"
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-          title="YouTube Video"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      )}
+      <button onClick={handlePlayVideo}>
+        <img src="./images/utils/iconPlay.png" />
+      </button>
     </div>
   );
 };
